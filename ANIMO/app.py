0de003aec0b9cb0d2081d12b03af1341b591853d
Austreleyros <144257068+Austreleyros@users.noreply.github.com>
2026-04-19 14:49:03 +0800

    app.py

--- ANIMO/app.py
+++ ANIMO/app.py
@@ -15,16 +15,16 @@ def home():
 def save_journal():
     data = request.json
     temp_journals.append({
-        "username": data.get('username', 'anonymous'),
+        "username": data.get('username', 'Anonymous'),
         "text": data.get('text'),
-        "image": data.get('image'), # NEW: stores the drawing
+        "image": data.get('image'), # stores the drawing
         "date": datetime.now().strftime("%B %d, %Y - %I:%M %p")
     })
-    return jsonify({"status": "success", "message": "journal received securely"}), 201
+    return jsonify({"status": "success", "message": "Journal received securely."}), 201
 
 @app.route('/api/journal', methods=['GET'])
 def get_journals():
-    username = request.args.get('username', 'anonymous user')
+    username = request.args.get('username', 'Anonymous User')
     user_journals = [j for j in temp_journals if j['username'] == username]
     return jsonify(user_journals), 200
 
@@ -32,11 +32,11 @@ def get_journals():
 def save_quiz():
     data = request.json
     temp_quizzes.append({
-        "username": data.get('username', 'anonymous'),
+        "username": data.get('username', 'Anonymous'),
         "score": data.get('score'),
         "recommendation": data.get('recommendation')
     })
-    return jsonify({"status": "success", "message": "quiz compiled"}), 201
+    return jsonify({"status": "success", "message": "Quiz compiled."}), 201
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
